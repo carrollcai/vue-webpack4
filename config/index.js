331fd7f0cc449f35e246030e1c94e93046dcd583
caicn <caicn@asiainfo.com>
2018-06-22 16:25:29 +0800

    打包改为相对路径1

--- config/index.js
+++ config/index.js
@@ -50,7 +50,9 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
+
+    /* 20180622修改打包路径为相对 */
+    assetsSubDirectory: './static',
 
     /* 20180622修改打包路径为相对 */
     assetsPublicPath: './',
