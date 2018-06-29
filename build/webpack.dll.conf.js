198c3ad4b852772bb7a2fbc2167994a0942e7f74
caicn <caicn@asiainfo.com>
2018-06-29 10:31:17 +0800

    完善dll打包，彻底解决vue.esm.js和element框架兼容问题

--- build/webpack.dll.conf.js
+++ build/webpack.dll.conf.js
@@ -10,11 +10,15 @@ const AssetsPlugin = require('assets-webpack-plugin');
 module.exports = {
   entry: {
     vendor: [
-      'vue',
+      /* 这里不能使用vue打包，因为vue打包默认的是使用vue.js文件， 基于 UMD 的完整构建，在版本上会和element-ui组件冲突
+      * 参考链接 https://forum.vuejs.org/t/vue-warn-attrs-is-readonly/18053/7
+      * 用vue打包，最终webpack build的时候，还是会把vue.esm.js（31KB）打包进去。基于 ES Module 的完整构建
+      * 用vue.esm.js打包，这里vue.esm.js比正常打包vue多了31kb，等于提前用ES Module构建。所以两者没有区别
+      */
+      'vue/dist/vue.esm.js',
       'vue-router',
       'vuex',
-      /* element-ui打包的时候有问题，是element-ui不兼容当前vue.esm.js版本，解决方式，应该是指定打包版本  */
-      // 'element-ui',
+      'element-ui',
       'axios',
       '@antv/g2',
       '@antv/data-set',
