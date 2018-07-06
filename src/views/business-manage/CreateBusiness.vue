23b6162633c0b0aa31ccf45d6e278c16e05c5ecd
liufulei <1653449366@qq.com>
2018-07-06 11:32:27 +0800

    走访和商机字段

--- src/views/business-manage/CreateBusiness.vue
+++ src/views/business-manage/CreateBusiness.vue
@@ -11,8 +11,8 @@
     <div class="container">
       <div>
       <el-form :rules="rules" ref="businessForm" :model="form" label-width="140px">
-        <el-form-item label="商机类别：" prop="category">
-          <el-select class="form-input-medium" v-model="form.category" placeholder="请选择属性">
+        <el-form-item label="商机类别：" prop="opporType">
+          <el-select class="form-input-medium" v-model="form.opporType" placeholder="请选择属性">
               <el-option
               v-for="item in businessCategoryList"
               :key="item.value"
@@ -21,16 +21,16 @@
               </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="预计收入：" prop="income">
-          <el-input v-model="form.income" class="form-input-medium" placeholder="请输入预计收入">
+        <el-form-item label="预计收入：" prop="predictContractAmount">
+          <el-input v-model="form.predictContractAmount" class="form-input-medium" placeholder="请输入预计收入">
             <template slot="append">万元/月</template>
           </el-input>
         </el-form-item>
-        <el-form-item label="预计签约时间：" prop="signTime">
-          <el-date-picker class="form-input-medium" format="yyyy-MM-dd" value-format="yyyy-MM-dd" type="date" v-model="form.signTime" placeholder="请选择时间"></el-date-picker>
+        <el-form-item label="预计签约时间：" prop="predictSignTime">
+          <el-date-picker class="form-input-medium" format="yyyy-MM-dd" value-format="yyyy-MM-dd" type="date" v-model="form.predictSignTime" placeholder="请选择时间"></el-date-picker>
         </el-form-item>
-        <el-form-item label="预计协议期：" prop="protoTime">
-          <el-select class="form-input-medium" v-model="form.protoTime" placeholder="请选择">
+        <el-form-item label="预计协议期：" prop="predictAgreementTime">
+          <el-select class="form-input-medium" v-model="form.predictAgreementTime" placeholder="请选择">
               <el-option
               v-for="item in protoTimeList"
               :key="item.value"
@@ -39,15 +39,15 @@
               </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="项目是否招标：" prop="tender">
-          <el-radio v-model="form.tender" label="1">是</el-radio>
-          <el-radio v-model="form.tender" label="2">否</el-radio>
+        <el-form-item label="项目是否招标：" prop="isProjectInvitation">
+          <el-radio v-model="form.isProjectInvitation" label="1">是</el-radio>
+          <el-radio v-model="form.isProjectInvitation" label="0">否</el-radio>
         </el-form-item>
-        <el-form-item label="联系人员：" prop="name">
-          <el-input maxlength="6" class="form-input-80" v-model="form.name" placeholder="姓名"></el-input>
+        <el-form-item label="联系人员：" prop="contactName">
+          <el-input maxlength="6" class="form-input-80" v-model="form.contactName" placeholder="姓名"></el-input>
           <span class="form-input-sep">-</span>
-          <el-form-item prop="sex" style="display: inline-block;">
-            <el-select class="form-input-80" v-model="form.sex" placeholder="性别">
+          <el-form-item prop="contactGender" style="display: inline-block;">
+            <el-select class="form-input-80" v-model="form.contactGender" placeholder="性别">
                 <el-option
                 v-for="item in options"
                 :key="item.value"
@@ -57,33 +57,33 @@
             </el-select>
           </el-form-item>
           <span class="form-input-sep">-</span>
-          <el-form-item prop="tel" style="display: inline-block;">
-            <el-input maxlength="11" class="form-input-120" v-model="form.tel" placeholder="手机号"></el-input>
+          <el-form-item prop="contactMobile" style="display: inline-block;">
+            <el-input maxlength="11" class="form-input-120" v-model="form.contactMobile" placeholder="手机号"></el-input>
           </el-form-item>
         </el-form-item>
-        <el-form-item label="联系邮箱：" prop="email">
-            <el-input maxlength="35" class="form-input-320" v-model="form.email" placeholder="请输入邮箱"></el-input>
+        <el-form-item label="联系邮箱：" prop="contactEmail">
+            <el-input maxlength="35" class="form-input-320" v-model="form.contactEmail" placeholder="请输入邮箱"></el-input>
           </el-form-item>
-        <el-form-item label="合作集团：" prop="group">
-          <el-autocomplete maxlength="25" class="form-input-half" v-model="form.group" :fetch-suggestions="querySearchAsync" placeholder="合作集团/编码" @select="handleSelect" @blur="noData = false;"></el-autocomplete>
+        <el-form-item label="合作集团：" prop="organizeName">
+          <el-autocomplete maxlength="25" class="form-input-half" v-model="form.organizeName" :fetch-suggestions="querySearchAsync" placeholder="合作集团/编码" @select="handleSelect" @blur="noData = false;"></el-autocomplete>
           <el-card class="create-business-box-card" v-if="noData">
             <div>
               系统暂未录入该集团，你可以暂时手动输入，建议后续尽快录入并同步关联修改！
             </div>
           </el-card>
           <span class="form-input-sep">-</span>
-          <el-form-item prop="office" style="display:inline-block;">
-            <el-input maxlength="50" class="form-input-half" v-model="form.office" placeholder="办公地址"></el-input>
+          <el-form-item prop="address" style="display:inline-block;">
+            <el-input maxlength="50" class="form-input-half" v-model="form.address" placeholder="办公地址"></el-input>
           </el-form-item>
         </el-form-item>
-        <el-form-item label="业务描述：" prop="desc">
-          <el-input maxlength="500" resize="none" class="form-input-320" type="textarea" :rows="3" placeholder="请输入业务描述" v-model="form.desc"></el-input>
+        <el-form-item label="业务描述：" prop="busiDesc">
+          <el-input maxlength="500" resize="none" class="form-input-320" type="textarea" :rows="3" placeholder="请输入业务描述" v-model="form.busiDesc"></el-input>
         </el-form-item>
-        <el-form-item label="业务需求：" prop="command">
-          <el-input class="form-input-320" type="textarea" :rows="3" placeholder="请输入业务需求" v-model="form.command"></el-input>
+        <el-form-item label="业务需求：" prop="busiRequire">
+          <el-input class="form-input-320" type="textarea" :rows="3" placeholder="请输入业务需求" v-model="form.busiRequire"></el-input>
         </el-form-item>
         <el-form-item label="需要协调的问题：">
-          <el-input maxlength="500" class="form-input-320" type="textarea" :rows="3" placeholder="请输入需要协调的问题" v-model="form.problem"></el-input>
+          <el-input maxlength="500" class="form-input-320" type="textarea" :rows="3" placeholder="请输入需要协调的问题" v-model="form.needCoordinationIssue"></el-input>
         </el-form-item>
       </el-form>
       </div>
@@ -93,7 +93,7 @@
       <div class="b-container">
           <el-form label-width="140px" style="width: 460px;">
             <el-form-item label="提醒人设置：">
-              <el-select class="form-input-medium" v-model="form.linkman" placeholder="请选择提醒人" multiple>
+              <el-select class="form-input-medium" v-model="form.reminders" placeholder="请选择提醒人" multiple>
                   <el-option
                   v-for="item in designatePerson"
                   :key="item.value"
@@ -121,27 +121,27 @@ export default {
   data() {
     return {
       form: {
-        category: '',
-        income: '',
-        signTime: '',
-        protoTime: '',
-        tender: '1',
-        name: '',
-        sex: '',
-        tel: '',
-        email: '',
-        group: '',
-        office: '',
-        desc: '',
-        command: '',
-        problem: '',
-        linkman: ''
+        opporType: '',
+        predictContractAmount: '',
+        predictSignTime: '',
+        predictAgreementTime: '',
+        isProjectInvitation: '0',
+        contactName: '',
+        contactGender: '',
+        contactMobile: '',
+        contactEmail: '',
+        organizeName: '',
+        address: '',
+        busiDesc: '',
+        busiRequire: '',
+        needCoordinationIssue: '',
+        reminders: ''
       },
       resetForm: {},
       businessCategoryList: [
         { 'label': '公司级商机', 'value': '0' },
-        { 'label': '分公司级商机', 'value': '1' },
-        { 'label': '普通商机', 'value': '2' }
+        { 'label': '分公司级商机', 'value': '2' },
+        { 'label': '普通商机', 'value': '1' }
       ],
       options: [
         { 'label': '男', 'value': '0' },
@@ -153,46 +153,46 @@ export default {
         { 'label': '3年（含）以上', 'value': '2' }
       ],
       rules: {
-        category: [
+        opporType: [
           { required: true, message: '请选择商机类别', trigger: ['blur', 'change'] }
         ],
-        group: [
+        organizeName: [
           { required: true, message: '请输入合作集团/编码', trigger: ['blur', 'change'] }
         ],
-        office: [
+        address: [
           { required: true, message: '请输入办公地址', trigger: ['blur', 'change'] }
         ],
-        name: [
+        contactName: [
           { required: true, message: '请输入姓名', trigger: ['blur', 'change'] }
         ],
-        sex: [
+        contactGender: [
           { required: true, message: '请选择性别', trigger: ['blur', 'change'] }
         ],
-        tel: [
+        contactMobile: [
           { required: true, message: '请输入手机号码', trigger: ['blur', 'change'] },
           { validator: checkPhone, trigger: 'blur' }
         ],
-        email: [
+        contactEmail: [
           { required: true, message: '请输入电子邮箱', trigger: ['blur', 'change'] },
           { validator: emailCheck, trigger: ['blur', 'change'] }
         ],
-        desc: [
+        busiDesc: [
           { required: true, message: '请输入业务描述', trigger: ['blur', 'change'] }
         ],
-        command: [
+        busiRequire: [
           { required: true, message: '请输入业务需求', trigger: ['blur', 'change'] }
         ],
-        income: [
+        predictContractAmount: [
           { required: true, message: '请输入预计收入', trigger: ['blur', 'change'] },
           { validator: inte5Deci4, trigger: ['blur', 'change'] }
         ],
-        signTime: [
+        predictSignTime: [
           { required: true, message: '请选择预计签约时间', trigger: ['blur', 'change'] }
         ],
-        protoTime: [
+        predictAgreementTime: [
           { required: true, message: '请选择预计协议期', trigger: ['blur', 'change'] }
         ],
-        tender: [
+        isProjectInvitation: [
           { required: true, message: '请选择项目是否招标', trigger: ['blur', 'change'] }
         ]
       },
@@ -250,7 +250,9 @@ export default {
       const params = this.form;
       this.$refs['businessForm'].validate(valid => {
         if (!valid) return false;
+        debugger;
         this.saveBusinessDraft(params).then(res => {
+          debugger;
           this.reset();
         });
       });
---
+++ src/views/visit/MyVisitView.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="m-container">
+      <div class="breadcrumb">
+        <el-breadcrumb>
+          <el-breadcrumb-item :to="{ path: '/visit/my-visit-manage' }">我的走访管理</el-breadcrumb-item>
+          <el-breadcrumb-item>执行处理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+    </div>
+    <div class="m-container visit-execute-process">
+      <div>
+        <handle-bar :title="['走访状态：', '审核人：']" :content="['待审核', '张三疯']" />
+        <handle-content />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import HandleBar from 'components/visit/HandleBar.vue';
+import HandleContent from 'components/visit/HandleContent.vue';
+
+export default {
+  data() {
+    return {
+      forwarderPerson: [],
+      form: {
+        forwarder: ''
+      }
+    };
+  },
+  components: {
+    HandleBar,
+    HandleContent
+  },
+  created() {
+  },
+  methods: {
+    ...mapActions([
+    ])
+  }
+};
+</script>
+
+<style lang="scss">
+.visit-execute-process {
+  margin-top: 16px;
+}
+.handle-visit-execute-process {
+  & .el-form-item__label {
+    color: rgba(0, 0, 0, 0.45);
+  }
+}
+.margin-right-8 {
+  margin-right: 8px;
+}
+.visit-execute-body {
+  border-radius: 2px;
+  background-color: #fafafa;
+  padding: 12px 12px;
+}
+</style>
