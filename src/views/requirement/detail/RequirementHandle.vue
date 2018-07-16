4c90b004d92f554b4f5bb9cf95e90ef6d1833a2a
jihh <jihh@asiainfo.com>
2018-07-16 15:56:08 +0800

    处理客户需求

--- src/views/requirement/detail/RequirementHandle.vue
+++ src/views/requirement/detail/RequirementHandle.vue
@@ -2,7 +2,7 @@
   <div class="requirement-detail-handle">
     <div class="m-container">
       <el-breadcrumb separator="/">
-        <el-breadcrumb-item :to="{ path: '/requirement/manage' }">需求创建管理</el-breadcrumb-item>
+        <el-breadcrumb-item :to="{ path: '/requirement/list' }">需求任务管理</el-breadcrumb-item>
         <el-breadcrumb-item>处理</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
@@ -11,55 +11,73 @@
     </div>
 
     <div class="m-container info-block">
-      <el-form>
+      <el-form
+        ref="baseForm"
+        :rules="rules"
+        :model="form"
+        label-width="130px"
+        >
         <!-- 物料需求处理 -->
         <template v-if="requirement.reqType === '2'">
-          <el-form-item lable="物料上传">
+          <el-form-item label="物料上传" prop="uploadFiles" key="material-upload">
+            <el-upload
+              :limit="5"
+              class="upload-demo"
+              :on-change="handleChange"
+              :on-remove="handleRemove"
+              :auto-upload="false"
+              :file-list="uploadFiles">
+              <el-button size="small" type="primary">选择文件</el-button>
+              <div slot="tip" class="el-upload__tip">
+                1、附件格式支持word、excel、ppt、pdf、rar格式<br/>
+                2、附件大小不超过20M。
+              </div>
+            </el-upload>
           </el-form-item>
-          <el-form-item lable="备注">
+          <el-form-item label="备注" required prop="materialDesc" key="materialDesc">
             <el-input class="col-input"
               type="textarea"
-              v-model="requirement.desc"
+              v-model="form.materialDesc"
               placeholder="备注"
-              key="contact-name-input-desc"></el-input>
+              key="material-desc-input-desc"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary">提交处理</el-button>
+            <el-button type="primary" @click="submitForm">提交处理</el-button>
           </el-form-item>
         </template>
 
         <!-- 日常、投诉需求处理  -->
         <template v-if="requirement.reqType === '0' || requirement.reqType === '1'">
-          <el-form-item lable="处理方式">
-            <el-radio-group v-model="handleType">
+          <el-form-item label="处理方式" required>
+            <el-radio-group v-model="handleType" @change="handleChangeType">
               <el-radio label="3">本人处理</el-radio>
               <el-radio label="6">投诉升级 <span>（转交给更高级级别的人处理）</span></el-radio>
             </el-radio-group>
           </el-form-item>
 
           <template v-if="handleType === '3'">
-            <el-form-item lable="处理方案">
+            <el-form-item label="处理方案" required prop="plan" key="plan">
               <el-input class="col-input"
                 type="textarea"
-                v-model="requirement.name"
+                v-model="form.plan"
                 placeholder="简要描述一下处理方案"
                 :maxlength="6"
-                key="contact-name-input"></el-input>
+                key="plan-input"></el-input>
             </el-form-item>
-            <el-form-item lable="备注">
+            <el-form-item label="备注" required prop="planDesc" key="plan-desc">
               <el-input class="col-input"
-                v-model="requirement.name"
+                v-model="form.planDesc"
                 type="textarea"
                 placeholder="备注"
                 :maxlength="6"
-                key="contact-name-input"></el-input>
+                key="plan-desc-input"></el-input>
             </el-form-item>
           </template>
 
           <template v-if="handleType === '6'">
-            <el-form-item label="指派处理人">
+            <el-form-item label="指派处理人" required key="processor">
               <el-col :span="8">
-                <el-form-item prop="processor" key="contact-name1">
+                <el-form-item prop="processor" key="processor-item">
                   <el-cascader
                     expand-trigger="hover"
                     :options="assignHandlers"
@@ -73,7 +91,7 @@
             </el-form-item>
           </template>
           <el-form-item>
-            <el-button type="primary">提交处理</el-button>
+            <el-button type="primary" @click="submitForm">提交处理</el-button>
           </el-form-item>
         </template>
       </el-form>
@@ -82,15 +100,70 @@
 </template>
 <script>
 import {mapActions, mapState} from 'vuex';
+import endsWith from 'lodash/endsWith';
 import mixins from './mixins';
 export default {
   name: 'RequirementHandle',
   mixins: [mixins],
   data() {
+    const that = this;
+    const filesValidator = function(rule, val, callback) {
+      const {uploadFiles} = that;
+      if (!uploadFiles.length) {
+        callback(new Error('请上传需求附件'));
+      } else {
+        callback();
+      }
+    };
+
+    const processorValidator = function(rule, val, callback) {
+      const {form} = that;
+      if (!form.processor) {
+        callback(new Error('请选择指派处理人'));
+      } else {
+        callback();
+      }
+    };
     return {
       handleType: '',
       checked: false,
-      processor: []
+      processor: [],
+      uploadFiles: [],
+      ACCEPT: [
+        '.rar',
+        '.docx',
+        '.doc',
+        '.ppt',
+        '.pptx',
+        '.xls',
+        '.xlsx',
+        '.pdf'
+      ],
+      form: {
+        uploadFiles: []
+      },
+      rules: {
+        uploadFiles: [
+          {
+            required: true,
+            validator: filesValidator,
+            trigger: 'change'
+          }
+        ],
+        materialDesc: [
+          { required: true, message: '请输入备注', trigger: 'blur' }
+        ],
+
+        plan: [
+          { required: true, message: '请输入处理方案', trigger: 'blur' }
+        ],
+        planDesc: [
+          { required: true, message: '请输入备注', trigger: 'blur' }
+        ],
+        processor: [
+          { validator: processorValidator, trigger: 'change' }
+        ]
+      }
     };
   },
   computed: {
@@ -102,11 +175,83 @@ export default {
     this.getAssignhandler();
   },
   watch: {
-    processor() {
-
+    processor(newVal) {
+      this.form.processor = newVal && newVal.length ? newVal[newVal.length - 1] : '';
     }
   },
   methods: {
+    isAcceptable(fileName) {
+      const {ACCEPT} = this;
+
+      for (let accept of ACCEPT) {
+        if (endsWith(fileName, accept)) {
+          return true;
+        }
+      }
+
+      return false;
+    },
+    handleChange(file, fileList) {
+      let fileName = file.name;
+      let result = true;
+      if (this.isAcceptable(fileName)) {
+        let fileSize = file.size / (1024 * 1024);
+
+        if (fileSize > 20) {
+          this.$message({
+            message: '附件超过20M',
+            type: 'error'
+          });
+
+          result = false;
+        } else {
+          this.uploadFiles.push(file.raw);
+        }
+      } else {
+        this.$message({
+          message: '只支持word、excel、ppt、pdf、rar格式',
+          type: 'error'
+        });
+        result = false;
+      }
+
+      if (!result) {
+        fileList.pop();
+      } else {
+        this.$refs.baseForm.validateField('uploadFiles');
+      }
+
+      return result;
+    },
+    handleRemove(file, fileList) {
+      const that = this;
+      const {uploadFiles} = that;
+
+      uploadFiles.splice(0, uploadFiles.length);
+
+      for (let item of fileList) {
+        uploadFiles.push(item.raw);
+      }
+
+      this.$refs.baseForm.validateField('uploadFiles');
+    },
+    handleChangeType(val) {
+      const {form} = this;
+      if (val === '6') {
+        form.plan = '';
+        form.planDesc = '';
+      } else {
+        form.processor = '';
+        this.processor = '';
+      }
+    },
+    submitForm() {
+      this.$refs.baseForm.validate((valid) => {
+        if (valid) {
+
+        }
+      });
+    },
     ...mapActions([
       'getAssignhandler'
     ])
