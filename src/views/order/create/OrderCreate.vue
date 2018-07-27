130570ab5a8354dc03a967a19765c4e19b18959e
caicn <396102647@qq.com>
2018-07-27 14:21:37 +0800

    订单地址限制50字符

--- src/views/order/create/OrderCreate.vue
+++ src/views/order/create/OrderCreate.vue
@@ -81,7 +81,7 @@
 <script>
 import { mapState, mapActions, mapMutations } from 'vuex';
 import { PAGE_SIZE } from '@/config/index.js';
-import { checkPhone, emailCheck, textLimit, textareaMaxLimit, inte5Deci4, textAccountLimit } from '@/utils/rules.js';
+import { checkPhone, emailCheck, textLimit, textareaLimit, textareaMaxLimit, inte5Deci4, textAccountLimit } from '@/utils/rules.js';
 
 export default {
   data() {
@@ -136,7 +136,7 @@ export default {
         ],
         address: [
           { required: true, message: '请输入地址', trigger: 'change' },
-          { validator: textAccountLimit, trigger: 'blur' }
+          { validator: textareaLimit, trigger: 'blur' }
         ],
         productName: [
           { required: true, message: '请输入产品名称', trigger: 'blur' },
