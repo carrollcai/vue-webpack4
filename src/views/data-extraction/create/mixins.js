742deb1eb8e30f78f4256bcbd253a1d0ac1767f2
jiazhen <1qaz!QAZ>
2018-08-30 16:14:52 +0800

    ## 数据提取管理_数据提取申请：明细数据标签设置有问题

--- src/views/data-extraction/create/mixins.js
+++ src/views/data-extraction/create/mixins.js
@@ -96,6 +96,7 @@ export default {
         let df = this.applyFrom.detailSet.indexOf('使用时长');
         if (df > -1) {
           this.applyFrom.detailSet.splice(df, 1);
+          this.applyFrom.serviceTime = [];
         }
       } else {
         this.applyFrom.isUseTime = false;
